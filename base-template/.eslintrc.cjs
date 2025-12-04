cf5cba6b9eae3777693c08b408bd137a20713851
Michael Bastos <bastosmichael@gmail.com>
2025-12-04 15:49:54 -0600

    Add base template TypeScript scaffold

---
+++ base-template/.eslintrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+};
